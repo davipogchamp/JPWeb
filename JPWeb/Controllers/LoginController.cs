7c65c6ef1d4997d19ade6705e227d646347e4a1b
davipogchamp <daviassini04@gmail.com>
2025-09-12 21:31:39 -0300

    Adicionar arquivos de projeto.

---
+++ JPWeb/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using JPWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace JPWeb.Controllers
+
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ValidaUsuario(Usuario usuario)
+        {
+            try
+            {
+                if (usuario.Email == "davi@gmail.com" && usuario.Senha == "davi123")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["MensagemErro"] = "Usuário ou senha inválidos!";
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            return View();
+        }
+    }
+}
